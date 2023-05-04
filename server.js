8ca11567cadb5ffe9517bbd025e74b65c5cf6bd8
rizkiprass <rizkiprass@gmail.com>
2023-05-04 21:04:55 +0700

    version8-test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World-v7-test');
+  res.send('Hello World-v8-test');
 });
 
 app.listen(PORT, HOST, () => {
