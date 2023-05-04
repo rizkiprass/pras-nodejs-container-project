42f8ce42f4188b397ce66b5c8f61604b8fae6cdb
rizkiprass <rizkiprass@gmail.com>
2023-05-04 21:34:38 +0700

    version10-test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World-v9-test');
+  res.send('Hello World-v10-test');
 });
 
 app.listen(PORT, HOST, () => {
