00dfd5c0a6487f8e6fc99c6bca11d66b1cd54e3b
Rizki Prasetya <rizkiprass@gmail.com>
2023-05-04 13:42:54 +0700

    v3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World2');
+  res.send('Hello World-v3');
 });
 
 app.listen(PORT, HOST, () => {
