34a744804652379b7dd17a0273979d55474cbe2e
rizkiprass <rizkiprass@gmail.com>
2023-05-05 14:57:55 +0700

    add rds connection

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 'use strict';
 
 const express = require('express');
+const mysql = require('mysql');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const DB_HOST = 'your-rds-endpoint-here';
+const DB_USER = 'your-db-username-here';
+const DB_PASSWORD = 'your-db-password-here';
+const DB_NAME = 'your-db-name-here';
+
+// Create MySQL connection pool
+const pool = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME
+});
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Hello World</title>
-      </head>
-      <body>
-        <h1>version-16-test</h1>
-      </body>
-    </html>
-  `);
+  // Use MySQL connection pool to execute query
+  pool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) {
+      res.send('Error connecting to database');
+    } else {
+      res.send(`
+        <html>
+          <head>
+            <title>Hello World</title>
+          </head>
+          <body>
+            <h1>Hello World-v16-test</h1>
+            <p>The database connection test was successful: ${results[0].solution}</p>
+          </body>
+        </html>
+      `);
+    }
+  });
 });
 
 app.listen(PORT, HOST, () => {
