e5d34ddc641740fcf15ccca5e8e5df06dfd7efa4
rizkiprass <rizkiprass@gmail.com>
2023-05-06 14:09:22 +0700

    testing-db

--- server.js
+++ server.js
@@ -1,24 +1,52 @@
 'use strict';
 
 const express = require('express');
+const mysql = require('mysql');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// MySQL config
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ', err);
+    return;
+  }
+  console.log('Connected to MySQL as ID ', connection.threadId);
+});
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Hello World</title>
-      </head>
-      <body>
-        <h1>version-17-test-reverted</h1>
-      </body>
-    </html>
-  `);
+  // Query the MySQL database
+  connection.query('SELECT * FROM mytable', (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ', err);
+      res.status(500).send('Error executing MySQL query');
+      return;
+    }
+    // Send the query results as a response
+    res.send(`
+      <html>
+        <head>
+          <title>Hello World</title>
+        </head>
+        <body>
+          <h1>version-17-test-reverted</h1>
+          <pre>${JSON.stringify(results, null, 2)}</pre>
+        </body>
+      </html>
+    `);
+  });
 });
 
 app.listen(PORT, HOST, () => {
